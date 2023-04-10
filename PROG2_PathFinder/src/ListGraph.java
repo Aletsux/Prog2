877c5a36cc7bdc62a0fa7d8a42c6998699d13ea8
Aletsux <71812424+Aletsux@users.noreply.github.com>
2023-04-10 11:34:14 +0200

    Update ListGraph.java

--- PROG2_PathFinder/src/ListGraph.java
+++ PROG2_PathFinder/src/ListGraph.java
@@ -1,5 +1,3 @@
-import java.io.IOException;
-import java.net.StandardSocketOptions;
 import java.util.*;
 
 /* Suggestions:
@@ -11,47 +9,81 @@ ToDo:
  */
 
 public class ListGraph<N> {
-    LinkedList<N> nodeList = new LinkedList<>();
-    private Map<N, List<N>> adjacencyList = new HashMap<>();
+    LinkedList<N> existingNodes = new LinkedList<>();
+
+    //Tracks nodes and connected edges
+    private Map<N, List<Edge<N>>> adjacentNodes = new HashMap<>();
 
     public void add(N node) {
-        if(nodeList.contains(node)) {
+        if(existingNodes.contains(node)) {
             System.err.println("Error: Node already exists!");
         }
-        nodeList.add(node);
+        existingNodes.add(node);
     }
 
     //ToDo: Need to remove edges
     public void remove(N node) throws NoSuchElementException {
         try {
-            if(!nodeList.contains(node)) {
+            if(!existingNodes.contains(node)) {
                 throw new NoSuchElementException("Not an existing node!");
             }
-            nodeList.remove(node);
+            existingNodes.remove(node);
         } catch (NoSuchElementException e) {
             System.err.println("Error: " + e.getMessage());
         }
     }
 
     // Multiple exceptions need to be thrown
-    // 'bindName' - from user input
+    // 'bindName' - from user input?
     public void connect(N currentNode, N newNode, String bindName, int weight) {
-        Edge edge = new Edge(bindName, weight, newNode);
+        try {
+                //checks whether selected nodes exist
+                if(!existingNodes.contains(currentNode) || !existingNodes.contains(newNode)) {
+                    throw new NoSuchElementException("Node doesn't exist");
+                }
 
-        // dont know what im doin here...
-        //saves the state of the graph to the selected Node?
-        adjacencyList.put(currentNode, nodeList);
+                if(weight < 0) {
+                    throw new IllegalArgumentException("Invalid value");
+                }
 
+                if(adjacentNodes.containsKey(currentNode)) {
+                    throw new IllegalStateException("A conenction already exists");
+                }
+
+                //create instance of edge to connect nodes
+                Edge<N> edge = new Edge<>(bindName, weight, newNode);
+
+                //set list 'edges' to the key 'currentNode' from the map
+                List<Edge<N>> edges = adjacentNodes.get(currentNode);
+
+                //If list is empty create new list of Edges
+                if(edges == null) {
+                    edges = new ArrayList<>();
+                    adjacentNodes.put(currentNode, edges);
+                }
+                edges.add(edge);
+        }
+        catch (NoSuchElementException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+
+        catch (IllegalArgumentException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+
+        catch (IllegalStateException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
     }
 
     // returns amount of 'nodes'
     public double getNodes() {
-        return nodeList.stream().count();
+        return existingNodes.stream().count();
     }
 
 
     public String toString() {
-        return nodeList.toString();
+        return existingNodes.toString();
     }
 
 }
