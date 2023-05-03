841dd396d2bce8c89831cdf24ff54ec09333b298
Aletsux <71812424+Aletsux@users.noreply.github.com>
2023-05-03 23:13:31 +0200

    New: PathFinder Class för del 2

---
+++ PROG2_PathFinder/src/PathFinder.java
@@ -0,0 +1,13 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class PathFinder extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        
+    }
+}
