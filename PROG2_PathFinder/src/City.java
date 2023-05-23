2dc99bde0dcefce9d6c4136a998c91429110bcfd
Aletsux <71812424+Aletsux@users.noreply.github.com>
2023-05-23 15:20:36 +0200

    Update City.java
    - new constructor, name

--- PROG2_PathFinder/src/City.java
+++ PROG2_PathFinder/src/City.java
@@ -1,34 +1,43 @@
 import javafx.scene.shape.Circle;
 
 import java.util.Objects;
+
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 
-public class City <N> extends Circle {
+public class City<N> extends Circle {
+    private String name = "";
 
     public City(double x, double y, double radius, Paint fill) {
         super(x, y, radius);
         setFill(Color.BLUE);
     }
 
+    public City(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 
 
+    @Override
+    public boolean equals(Object other) {
+        if (other instanceof City city) {
+            return name.equals(city.name);
+        } else {
+            return false;
+        }
+    }
 
-//    @Override
-//    public boolean equals (Object other) {
-//        if (other instanceof City city) {
-////            return name.equals(city.name);
-//        } else {
-//            return false;
-//        }
-//    }
-//    @Override
-//    public int hashCode(){
-//        return Objects.hash(name);
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return name;
-//    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
