1f8fbca94353aef0f4bac2c41e7d8c050f99b8db
Aletsux <71812424+Aletsux@users.noreply.github.com>
2023-06-03 18:47:32 +0200

    Update TestClass.java

--- PROG2_PathFinder/src/TestClass.java
+++ PROG2_PathFinder/src/TestClass.java
@@ -11,27 +11,27 @@ public class TestClass {
     * I1 - 'NullPointerException' cannot invoke java.util.Set.iterator (methods with problems: getEdgeBetween, disconnect)
 
     */
-    private static final City NODEONE = new City<>("New York", 100.0, 200.0);
-    private static final City NODETWO = new City<>("Stockholm", 200.0, 300.0);
-    private static final City NODETHREE = new City<>("Berlin", 300.0, 400.0);
-    private static final City NODEFOUR = new City<>("Tokyo", 400, 500);
-    private static final City NODEFIVE = new City<>("London", 500, 600);
+    private static final City NODEONE = new City<>("New York", 100, 100);
+    private static final City NODETWO = new City<>("Stockholm", 200, 200);
+    private static final City NODETHREE = new City<>("Berlin", 300, 300);
+    private static final City NODEFOUR = new City<>("Tokyo", 400, 400);
+    private static final City NODEFIVE = new City<>("London", 500, 500);
 
     //ListGraph listGraphClass = new ListGraph();
     //WIP
 
     ListGraph graph = new ListGraph();
     Edge edgeClass;
-    PathFinder pathFinder = new PathFinder();
+    //PathFinder pathFinder = new PathFinder();
 
     public static void main(String[] args) {
         TestClass testClass = new TestClass();
         testClass.loadData();
 
-        //testClass.testConnectMethod();
-        testClass.addSelectedNodes(PathFinder.selectedNodes);
+        testClass.testConnectMethod();
+        //testClass.addSelectedNodes(PathFinder.selectedNodes);
 
-        //testClass.disconnectEdge();
+        testClass.disconnectEdge();
         //testClass.printSet();
 
         //testClass.testEdge();
@@ -43,7 +43,7 @@ public class TestClass {
         //PathFinder.start();
     }
 
-    public ListGraph runTests() {
+    /*public ListGraph runTests() {
 
         if (pathFinder.getListGraph() == null) {
             System.err.println("graph is null");
@@ -52,9 +52,9 @@ public class TestClass {
         }
         loadData();
         testConnectMethod();
-        addSelectedNodes(pathFinder.selectedNodes);
+        //addSelectedNodes(pathFinder.selectedNodes);
         return graph;
-    }
+    }*/
 
 
     //Test functionality of connect method
@@ -64,11 +64,12 @@ public class TestClass {
         graph.connect(NODEONE, NODETHREE, "aConnection", 300);
         graph.connect(NODETWO, NODETHREE, "S -> B", 500);
         //graph.connect(NODEFOUR, NODEFIVE, "Flight", 12);
-        graph.connect(NODETWO, NODEFIVE, "S -> L", 300);
+        graph.connect(NODETWO, NODEFIVE, "S -> L", 400);
         graph.connect(NODEFIVE, NODEFOUR, "L -> T", 500);
 
         //Throws exception: IllegalStateException
         //listGraphClass.connect(NODEONE, NODETHREE, "cConnection", 800);
+        //System.out.println("Original: " + graph.printConnections());
     }
 
     public void loadData() {
@@ -85,8 +86,11 @@ public class TestClass {
     }
 
     private void disconnectEdge() {
-        graph.disconnect(NODETWO, NODETHREE);
+        System.out.println("Before: " + graph.printConnections());
+        graph.disconnect(NODETWO, NODETHREE); //S -> B
+        graph.disconnect(NODETWO, NODEFIVE); //One way connection -> selectable
         //listGraphClass.disconnect(NODEONE, NODETWO);
+        System.out.println("After: " + graph.printConnections());
     }
 
     private void printSet() {
